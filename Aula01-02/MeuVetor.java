6857d94302f3bbdbdad835210d12817dbf19ebf7
Mateus Wontroba <112833708+Wontsz@users.noreply.github.com>
2024-02-29 12:52:03 -0300

    aula 29/02
    completando o código de vetores da aula 1, adicionando e removendo a capacidade

--- Aula01-02/MeuVetor.java
+++ Aula01-02/MeuVetor.java
@@ -10,7 +10,7 @@ public class MeuVetor{
         ultimaPos = -1;
     }
 
-    // Método Get/ - buscando um valor
+    // Método Get/de acesso - buscando um valor
     public int getUltimaPos(){ //Getter do UltimaPos
         return ultimaPos;
     }
@@ -20,7 +20,7 @@ public class MeuVetor{
         return v;
     }
 
-    // Setters - receber/atribuir um valor a um atributo
+    // Método Setters/modificadores - receber/atribuir um valor a um atributo
     public void setUltimaPos(int pos){
         // estrutura condicional
         if(pos >= 0 && pos < v.length){ // .length = o tamanho do vetor
@@ -30,7 +30,7 @@ public class MeuVetor{
         }
     }
 
-    //22-2-24 
+    // Aula 02 22-2-24 
     public boolean estaCheio(){
         return ultimaPos == v.length-1;
     }
@@ -40,13 +40,48 @@ public class MeuVetor{
         return ultimaPos == -1;
     }
 
-    public boolean add(int elemento){
-        if(estaCheio()){
-            return false;
-        }else{
-            ultimaPos++; // ultimaPos = ultimaPos + 1
-            v[ultimaPos] = elemento;
-            return true;
+    public void adicionaNaPosicao(int elemento, int posicao){
+        v[posicao] = elemento;
+    }
+    private void redimensiona (int novaCapacidade){
+        int[] temp = new int[novaCapacidade];
+        for(int i=0; i<=ultimaPos; i++){
+            temp[i] = v[i];
+            v = temp;
         }
+    }
+    
+    public void add(int elemento){
+        if(estaCheio()){
+            redimensiona(v.length*2);
+        }
+        ultimaPos++; // ultimaPos = ultimaPos + 1
+        v[ultimaPos] = elemento;
+    }
+
+    // Aula 03 29-02-24
+
+    public int remove(){
+        if(estaVazio()){
+            return 0;
+        }
+        int aux = v[ultimaPos--];
+        if (v.length >= 10 && ultimaPos <= v.length/4) {
+            redimensiona(v.length/2);
+        }
+        return aux;
+    }
+    
+    @Override // metodo proprio de uma classe (sobrepor) sobrepoe um objeto (v1 por s)
+    public String toString(){
+        String s = "";
+        if(estaVazio()){
+            s = s + "esta vazio!";
+        }else{
+            for(int i=0; i<=ultimaPos; i++){
+                s = s + v[i] + " ";
+            }
+        }
+        return s + "\n";
     }
 }
--- Aula01-02/TesteVetor.java
+++ Aula01-02/TesteVetor.java
@@ -1,28 +1,40 @@
 // arquivo de inicio da compilaçao
 public class TesteVetor{
     public static void main(String[] args){
-        MeuVetor v1 = new MeuVetor(10); // vetor de 10 pos
+        MeuVetor v1 = new MeuVetor(4); // vetor de 10 pos
         //  Classe           Metodo Constr
         // imprime ultimaPos = -1
         System.out.println("Ultima posicao de pois de instanciar: " + v1.getUltimaPos());
 
-        v1.setUltimaPos(4);
-
+        v1.setUltimaPos(2);
         System.out.println("ultima posicao depois do parametro: " + v1.getUltimaPos());
+        System.out.println("vetor apos setUltimapos: " + v1);
         
-        //22-2-24
-        if(v1.add(12)){
-            System.out.println("Elemento inserido com sucesso!");
-            System.out.println("Ultima posição após atribuição: " + v1.getUltimaPos());
-        }else{
-            System.out.println("Vetor esta cheio!");
+        // 22-2-24 Aula 01
+        // if(v1.add(100)){
+        //     System.out.println("Elemento inserido com sucesso!");
+        //     System.out.println("Ultima posicao apos atribuicao: " + v1.getUltimaPos());
+        //     System.out.println("vetor apos setUltimapos: " + v1);
+        // }else{
+        //     System.out.println("Vetor esta cheio!");
+        // }
+
+        // 29/02-24 Aula 03
+
+        // adiciona um elemento em uma posição e imprime a ulitma posição
+        v1.adicionaNaPosicao(4, 2);
+        System.out.println("Ultima posicao apos atribuicao: " + v1.getUltimaPos());
+        System.out.println("vetor apos setUltimapos: " + v1);
+
+        // incluir elementos no vetor
+        for(int i=0; i<=100; i++){
+           v1.add(i);
+           System.out.println("adicionou: " + i + " capacidade: " + v1.getV().length); 
         }
 
-        if(v1.add(23)){
-            System.out.println("Elemento inserido com sucesso!");
-            System.out.println("Ultima posição após atribuição: " + v1.getUltimaPos());
-        }else{
-            System.out.println("Vetor está cheio!");
+        // Excluir elemntos do vetor e vai diminuir a capacidade
+        while(!v1.estaVazio()){  // ! é negação
+            System.out.println(v1.remove() + " foi removido, capacidade: " + v1.getV().length);
         }
     }
 }
